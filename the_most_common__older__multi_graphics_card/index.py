33df380188705bb103ffbf19f3d49a952a6fbef8
r13922a13 <r13922a13@puipui.csie.ntu.edu.tw>
2026-07-26 09:53:43 +0800

    strange 8 , what if i sw7

--- the_most_common__older__multi_graphics_card/index.py
+++ the_most_common__older__multi_graphics_card/index.py
@@ -13,11 +13,11 @@ from torch.utils.checkpoint import checkpoint_sequential
 torch.cuda.memory._record_memory_history()
 
 model = nn.Sequential(
-    nn.Linear(2, 3, bias = False, device='cuda:0'),
+    nn.Linear(7, 3, bias = False, device='cuda:0'),
     nn.Linear(3, 5, bias = False, device='cuda:1')  
     )
 
-x = torch.randn(2, 2,  device='cuda:0')
+x = torch.randn(2, 7,  device='cuda:0')
 
 balance = [1, 1]
 devices = ['cuda:0', 'cuda:1']
