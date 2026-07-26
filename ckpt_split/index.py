7cb264258ef64fbb65edb4f494db0b14b17267b5
defaultuser <default@example.com>
2026-07-26 08:14:35 +0800

    .

--- ckpt_split/index.py
+++ ckpt_split/index.py
@@ -12,14 +12,28 @@ from torch.utils.checkpoint import checkpoint_sequential
 
 torch.cuda.memory._record_memory_history()
 
-model = nn.Sequential(
-    nn.Linear(2, 7, bias = False, device='cuda'),
-    nn.Linear(7, 3, bias = False, device='cuda'),
-    nn.Linear(3, 5, bias = False, device='cuda')
+model = nn.Sequential(                           #             1*2*4   >
+    nn.Linear(2, 7, bias = False, device='cuda'),#2*7*4        > 7*4
+    nn.Linear(7, 3, bias = False, device='cuda'),#7*3*4        > 3*4  
+    nn.Linear(3, 5, bias = False, device='cuda'),#3*5*4        > 5*4
+
+    nn.Linear(5, 2, bias = False, device='cuda'),#5*2*4        > 2*4
+    nn.Linear(2, 3, bias = False, device='cuda'),#2*3*4        > 3*4
+    nn.Linear(3, 5, bias = False, device='cuda'),#3*5*4        > 5*4
+
+    nn.Linear(5, 7, bias = False, device='cuda'),#5*7*4        > 7*4
+    nn.Linear(7, 3, bias = False, device='cuda'),#7*3*4        > 3*4
+    nn.Linear(3, 5, bias = False, device='cuda') #3*5*4        > 5*4 
     )
 
 x = torch.randn(1, 2,  device='cuda')
 
+y = checkpoint_sequential(
+    model, 
+    segments=3, 
+    input=x,
+    use_reentrant=False
+    )
 
 loss = torch.ones_like(y)
 y.backward(loss)
