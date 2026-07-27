5ff5c346d6ab553a0dd09554ba3eaedbf52eac18
r13922a13 <r13922a13@puipui.csie.ntu.edu.tw>
2026-07-27 17:31:42 +0800

    .

--- sector22gpu/index.py
+++ sector22gpu/index.py
@@ -4,31 +4,35 @@ os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":0:0"
 
 from icecream import ic
 ic.configureOutput(includeContext=True)
+import sys
+import wandb
 
 import torch
 import torch.nn as nn
 from torchgpipe import GPipe
 from torch.utils.checkpoint import checkpoint_sequential
 
+wandb.init(project="https_r13922a13_lab503html_note_20260727", save_code=True)
+
 torch.cuda.memory._record_memory_history()
 
 model = nn.Sequential(
     nn.Linear(7, 3, bias = False, device='cuda:0'),
-    nn.Linear(3, 5, bias = False, device='cuda:1'),  
+    nn.Linear(3, 2, bias = False, device='cuda:1'),  
     nn.Linear(2, 3, bias = False, device='cuda:2'),
-    nn.Linear(3, 3, bias = False, device='cuda:3'),#~
+    #nn.Linear(3, 3, bias = False, device='cuda:3'),#~
     nn.Linear(3, 5, bias = False, device='cuda:1')
     )
 
 x = torch.randn(1, 7,  device='cuda:0')
 
-balance = [1, 1, 1, 1, 1]
-#devices = ['cuda:0', 'cuda:1']
+balance = [1, 1, 1, 1]
+devices = ['cuda:0', 'cuda:1', 'cuda:2']#, 'cuda:3', 'cuda:0']
 
 model = GPipe(
     model, 
     balance=balance, 
-    #devices=devices, 
+    devices=devices, 
     chunks=1, 
     checkpoint='never'
     )
@@ -39,3 +43,5 @@ loss = torch.ones_like(y)
 y.backward(loss)
 
 torch.cuda.memory._dump_snapshot('sg22.pickle')
+
+wandb.finish()
