1b54c9288d02a88f86028e546a74feb9d3f26af8
r13922a13 <r13922a13@puipui.csie.ntu.edu.tw>
2026-07-27 21:01:32 +0800

    我猜下一波不會續存

--- sector22gpu/index.py
+++ sector22gpu/index.py
@@ -19,16 +19,20 @@ torch.cuda.memory._record_memory_history()
 x = torch.randn(1, 7,  device='cuda:0')            #1*7*4=28
 model = nn.Sequential(
     nn.Linear(7, 2, bias = False, device='cuda:0'),#7*2*4=56 > 2*4=8
+    nn.Linear(2, 2, bias = False, device='cuda:0'),
     nn.Linear(2, 3, bias = False, device='cuda:0'),#2*3*4=24 > 3*4=12
     nn.Linear(3, 5, bias = False, device='cuda:0'),#3*5*4=60 > 5*4=20
+    nn.Linear(5, 5, bias = False, device='cuda:0'),
     nn.Linear(5, 2, bias = False, device='cuda:0'),#5*2*4=40 > 2*4=8
     nn.Linear(2, 7, bias = False, device='cuda:1'),#2*7*4=56 > 7*4=28
+    nn.Linear(7, 7, bias = False, device='cuda:1'),
     nn.Linear(7, 3, bias = False, device='cuda:1'),#7*3*4=84 > 3*4=12 
     nn.Linear(3, 2, bias = False, device='cuda:1'),#3*2*4=24 > 2*4=8
+    nn.Linear(2, 2, bias = False, device='cuda:1'),
     nn.Linear(2, 5, bias = False, device='cuda:1'),#2*5*4=40 > 5*4=20
     )
 
-balance = [2, 2, 2, 2]
+balance = [3, 3, 3, 3]
 devices = ['cuda:0', 'cuda:0', 'cuda:1', 'cuda:1']
 
 model = GPipe(
