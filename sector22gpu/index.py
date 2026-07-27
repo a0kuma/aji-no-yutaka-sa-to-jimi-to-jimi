dd4b4e98befb93b9d09f9b3fe8f3620bacbedce7
r13922a13 <r13922a13@puipui.csie.ntu.edu.tw>
2026-07-27 19:27:59 +0800

    差不多

--- sector22gpu/index.py
+++ sector22gpu/index.py
@@ -19,35 +19,27 @@ torch.cuda.memory._record_memory_history()
 model = nn.Sequential(
     nn.Linear(7, 3, bias = False, device='cuda:0'),#7*3*4=84 > 3*4=12
     nn.Linear(3, 2, bias = False, device='cuda:0'),#3*2*4=24 > 2*4=8
-    nn.Linear(2, 5, bias = False, device='cuda:0') #2*5*4=40 > 5*4=20
+    nn.Linear(2, 7, bias = False, device='cuda:1'),#2*7*4=56 > 7*4=28
+    nn.Linear(7, 5, bias = False, device='cuda:1'),#7*5*4=140 > 5*4=20
     )
 
 x = torch.randn(1, 7,  device='cuda:0')
 
-balance = [3]
-devices = ['cuda:0']
+balance = [2, 2]
+devices = ['cuda:0','cuda:1']
 
-#model = GPipe(
-    #model, 
-    #balance=balance, 
-    #devices=devices, 
-    #chunks=1, 
-    #checkpoint='always'
-    #) 
+model = GPipe(
+    model, 
+    balance=balance, 
+    devices=devices, 
+    chunks=1, 
+    checkpoint='always'
+    ) 
 
-y = checkpoint.checkpoint(
-    model,
-    input=x,
-    determinism_check='none',
-    debug=False,
-    early_stop=False,
-    use_reentrant=False
-    )
-
-#y = model(x)
+y = model(x)
 
 loss = torch.ones_like(y)
 y.backward(loss)
 
-torch.cuda.memory._dump_snapshot(f'official_ES_falsesg22{str(dt.datetime.now().timestamp()).replace(".","")}.pickle')
+torch.cuda.memory._dump_snapshot(f'mama{str(dt.datetime.now().timestamp()).replace(".","")}.pickle')
 
