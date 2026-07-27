ce9305e9183f4f19319d97723f1b94f4f4ab72df
r13922a13 <r13922a13@puipui.csie.ntu.edu.tw>
2026-07-27 16:51:01 +0800

    .

---
+++ sector22gpu/how_the_device_list_etc_work/index.py
@@ -0,0 +1,40 @@
+import os
+os.environ["CUBLASLT_WORKSPACE_SIZE"] = "0"
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":0:0"
+
+from icecream import ic
+ic.configureOutput(includeContext=True)
+
+import torch
+import torch.nn as nn
+from torchgpipe import GPipe
+from torch.utils.checkpoint import checkpoint_sequential
+
+torch.cuda.memory._record_memory_history()
+
+model = nn.Sequential(
+    nn.Linear(7, 3, bias = False, device='cuda:0'),
+    nn.Linear(3, 5, bias = False, device='cuda:0'),  
+    nn.Linear(2, 3, bias = False, device='cuda:1'),
+    nn.Linear(3, 5, bias = False, device='cuda:1')
+    )
+
+x = torch.randn(1, 7,  device='cuda:0')
+
+balance = [1, 1]
+devices = ['cuda:0', 'cuda:1']
+
+model = GPipe(
+    model, 
+    balance=balance, 
+    devices=devices, 
+    chunks=1, 
+    checkpoint='never'
+    )
+
+y = model(x)
+
+loss = torch.ones_like(y)
+y.backward(loss)
+
+torch.cuda.memory._dump_snapshot('sg22.pickle')
