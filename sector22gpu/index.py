74c43d5fb0fb6ad0235d5c0e8b99c2c56abb29d5
r13922a13 <r13922a13@puipui.csie.ntu.edu.tw>
2026-07-27 19:44:09 +0800

    torch.cuda.memory._dump_snapshot(f'layer8segment4gpu2{str(dt.datetime.now().timestamp()).replace(.,)}.pickle')

--- sector22gpu/index.py
+++ sector22gpu/index.py
@@ -16,17 +16,20 @@ from torch.utils.checkpoint import checkpoint_sequential
 
 torch.cuda.memory._record_memory_history()
 
+x = torch.randn(1, 7,  device='cuda:0')            #1*7*4=28
 model = nn.Sequential(
-    nn.Linear(7, 3, bias = False, device='cuda:0'),#7*3*4=84 > 3*4=12
-    nn.Linear(3, 2, bias = False, device='cuda:0'),#3*2*4=24 > 2*4=8
+    nn.Linear(7, 2, bias = False, device='cuda:0'),#7*2*4=56 > 2*4=8
+    nn.Linear(2, 3, bias = False, device='cuda:0'),#2*3*4=24 > 3*4=12
+    nn.Linear(3, 5, bias = False, device='cuda:0'),#3*5*4=60 > 5*4=20
+    nn.Linear(5, 2, bias = False, device='cuda:0'),#5*2*4=40 > 2*4=8
     nn.Linear(2, 7, bias = False, device='cuda:1'),#2*7*4=56 > 7*4=28
-    nn.Linear(7, 5, bias = False, device='cuda:1'),#7*5*4=140 > 5*4=20
+    nn.Linear(7, 3, bias = False, device='cuda:1'),#7*3*4=84 > 3*4=12 
+    nn.Linear(3, 2, bias = False, device='cuda:1'),#3*2*4=24 > 2*4=8
+    nn.Linear(2, 5, bias = False, device='cuda:1'),#2*5*4=40 > 5*4=20
     )
 
-x = torch.randn(1, 7,  device='cuda:0')
-
-balance = [2, 2]
-devices = ['cuda:0','cuda:1']
+balance = [2, 2, 2, 2]
+devices = ['cuda:0', 'cuda:0', 'cuda:1', 'cuda:1']
 
 model = GPipe(
     model, 
@@ -41,5 +44,5 @@ y = model(x)
 loss = torch.ones_like(y)
 y.backward(loss)
 
-torch.cuda.memory._dump_snapshot(f'mama{str(dt.datetime.now().timestamp()).replace(".","")}.pickle')
+torch.cuda.memory._dump_snapshot(f'layer8segment4gpu2{str(dt.datetime.now().timestamp()).replace(".","")}.pickle')
 
